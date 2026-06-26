89f819e3469b5b4f60aa75504af1694b0b44ab6b
gempir <gempir.dev@gmail.com>
2026-06-26 20:23:02 +0200

    new typescript version

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => ({
     server: {
         host: "127.0.0.1",
         port: 3000,
     },
+    resolve: {
+        tsconfigPaths: true,
+    },
     plugins: [
-        tsConfigPaths(),
         tanstackStart({
             spa: {
                 enabled: true,
